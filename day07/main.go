1515461cfaf1cd1fcc9b08ee5713afd72532bfc1
Scott Kerkvliet <scott_kerkvliet@hotmail.com>
2023-12-08 01:34:05 -0500

    Day 7 part 2

--- day07/main.go
+++ day07/main.go
@@ -41,68 +41,76 @@ func (h *Hand) IsBetterThan(o *Hand) bool {
 	return false
 }
 
-func readHand(line string) (*Hand, error) {
-	if len(line) < 7 {
-		return nil, fmt.Errorf("Line is too short for a hand %q", line)
-	}
-	cardString := line[0:5]
-	bidString := line[6:]
+func getHandReader(jokers bool) func(line string) (*Hand, error) {
+	return func(line string) (*Hand, error) {
+		if len(line) < 7 {
+			return nil, fmt.Errorf("Line is too short for a hand %q", line)
+		}
+		cardString := line[0:5]
+		bidString := line[6:]
 
-	bid, err := strconv.Atoi(bidString)
-	if err != nil {
-		return nil, fmt.Errorf("Error parsing bid: %w", err)
-	}
+		bid, err := strconv.Atoi(bidString)
+		if err != nil {
+			return nil, fmt.Errorf("Error parsing bid: %w", err)
+		}
 
-	var cards [5]int
-	cardMap := make(map[int]int)
-	for i, char := range cardString {
+		var cards [5]int
+		cardMap := make(map[int]int)
+		for i, char := range cardString {
+			switch {
+			case unicode.IsDigit(char):
+				cards[i] = int(char - '0')
+			case char == 'A':
+				cards[i] = 14
+			case char == 'K':
+				cards[i] = 13
+			case char == 'Q':
+				cards[i] = 12
+			case char == 'J' && jokers:
+				cards[i] = -1
+			case char == 'J':
+				cards[i] = 11
+			case char == 'T':
+				cards[i] = 10
+			default:
+				return nil, fmt.Errorf("Got unexpected rune in cards: %q", string(char))
+			}
+			cardMap[cards[i]]++
+		}
+
+		highestCount, secondHighestCount := 0, 0
+		for card, count := range cardMap {
+			if card == -1 {
+				continue
+			}
+			if count >= highestCount {
+				secondHighestCount = highestCount
+				highestCount = count
+			} else if count > secondHighestCount {
+				secondHighestCount = count
+			}
+		}
+		highestCount += cardMap[-1]
+
+		hand := &Hand{cards: cards, bid: bid}
 		switch {
-		case unicode.IsDigit(char):
-			cards[i] = int(char - '0')
-		case char == 'A':
-			cards[i] = 14
-		case char == 'K':
-			cards[i] = 13
-		case char == 'Q':
-			cards[i] = 12
-		case char == 'J':
-			cards[i] = 11
-		case char == 'T':
-			cards[i] = 10
+		case highestCount == 5:
+			hand.handType = Quintuple
+		case highestCount == 4:
+			hand.handType = Quadruple
+		case highestCount == 3 && secondHighestCount == 2:
+			hand.handType = FullHouse
+		case highestCount == 3:
+			hand.handType = Triple
+		case highestCount == 2 && secondHighestCount == 2:
+			hand.handType = TwoPair
+		case highestCount == 2:
+			hand.handType = Pair
 		default:
-			return nil, fmt.Errorf("Got unexpected rune in cards: %q", string(char))
+			hand.handType = HighCard
 		}
-		cardMap[cards[i]]++
+		return hand, nil
 	}
-
-	highestCount, secondHighestCount := 0, 0
-	for _, count := range cardMap {
-		if count >= highestCount {
-			secondHighestCount = highestCount
-			highestCount = count
-		} else if count > secondHighestCount {
-			secondHighestCount = count
-		}
-	}
-
-	hand := &Hand{cards: cards, bid: bid}
-	switch {
-	case highestCount == 5:
-		hand.handType = Quintuple
-	case highestCount == 4:
-		hand.handType = Quadruple
-	case highestCount == 3 && secondHighestCount == 2:
-		hand.handType = FullHouse
-	case highestCount == 3:
-		hand.handType = Triple
-	case highestCount == 2 && secondHighestCount == 2:
-		hand.handType = TwoPair
-	case highestCount == 2:
-		hand.handType = Pair
-	default:
-		hand.handType = HighCard
-	}
-	return hand, nil
 }
 
 func printCards(cards [5]int) (cardString string) {
@@ -125,30 +133,42 @@ func printCards(cards [5]int) (cardString string) {
 	return
 }
 
-func part1(file string) error {
-	hands, err := fileReader.ReadFileByLine(file, readHand)
-	if err != nil {
-		return err
-	}
-
+func getTotalWinnings(hands []*Hand) int {
 	sort.Slice(hands, func(i, j int) bool {
 		return !hands[i].IsBetterThan(hands[j])
 	})
 
-	var totalWinnings int64
+	var totalWinnings int
 	for i, hand := range hands {
 		rank := i + 1
 		winnings := hand.bid * rank
-		totalWinnings += int64(winnings)
+		totalWinnings += winnings
 		// fmt.Printf("Hand %v type %v bid %4d rank %4d winnings %v\n", printCards(hand.cards), hand.handType, hand.bid, rank, winnings)
 	}
+	return totalWinnings
+}
+
+func part1(file string) error {
+	hands, err := fileReader.ReadFileByLine(file, getHandReader(false))
+	if err != nil {
+		return err
+	}
+
+	totalWinnings := getTotalWinnings(hands)
 
 	fmt.Printf("The total winnings from part 1 are %v\n", totalWinnings)
 	return nil
 }
 
 func part2(file string) error {
-	fmt.Println("Part 2 is not implemented.")
+	hands, err := fileReader.ReadFileByLine(file, getHandReader(true))
+	if err != nil {
+		return err
+	}
+
+	totalWinnings := getTotalWinnings(hands)
+
+	fmt.Printf("The total winnings from part 2 are %v\n", totalWinnings)
 	return nil
 }
 
